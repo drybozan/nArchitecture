d4937e4e6f2cdef5faf353b30875176a3153b8bf
drybozan <derya7649@gmail.com>
2022-10-17 10:39:18 +0300

    Add project files.

---
+++ src/demoProjects/rentACar/Application/Services/Repositories/IBrandRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Persistence.Repositories;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Repositories
+{
+
+    // Brand e özel repositoryler için hazırlanan interface
+    public interface IBrandRepository: IAsyncRepository<Brand>, IRepository<Brand>
+    {
+
+    }
+}
